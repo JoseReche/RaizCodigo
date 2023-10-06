d5ddb978b2f656844fbd63dfd2e02d4c84a0de96
JoseReche <jose.carlosreche@gmail.com>
2023-10-05 22:17:04 -0300

    inicio em javaScript

---
+++ AdsWeb/JavaScript/aula01/JS/script.js
@@ -0,0 +1,10 @@
+alert('Olá JavaScrip');
+var idade = 44; // Global
+let nome = "José Carlos Reche"; // limita a variavel no escopo {}
+rua = "Rua almirante jaceguai"; //
+
+alert(idade);// PoPap
+alert(nome);
+alert(rua);
+
+console.log(idade, nome, rua);
